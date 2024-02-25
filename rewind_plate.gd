b96c92d3b77cffcc857eb57c49e89bed811a01d9
ryan cooper <3RCOOPER8@GMAIL.COM>
2024-02-25 17:47:49 -0500

    Animate sprites

--- rewind_plate.gd
+++ rewind_plate.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
-
+@onready var state_chart = $StateChart
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var _animation_state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+@onready var sprite_2d = $Sprite2D
 
 const SPEED = 700.0
 const JUMP_VELOCITY = -450.0
@@ -7,7 +11,8 @@ const JUMP_VELOCITY = -450.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func _ready():
+	$AnimationPlayer.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -23,9 +28,15 @@ func _physics_process(delta):
 	var direction = Input.get_axis("p2_left", "p2_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite_2d.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		sprite_2d.flip_h = false
+	if velocity.length_squared() <= 0.05:
+		state_chart.send_event("idling")
+	else:
+		state_chart.send_event("moving")
+		_animation_state_machine.travel("Moving")
 	move_and_slide()
 	#for index in get_slide_collision_count():
 		#var collision := get_slide_collision(index)
