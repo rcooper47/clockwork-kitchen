cabb6f8c6476b4b1875c48391ed1c7428219662e
ryan cooper <3RCOOPER8@GMAIL.COM>
2024-02-25 12:26:33 -0500

    Order timeout

--- WineOrder.gd
+++ WineOrder.gd
@@ -3,9 +3,11 @@ extends Sprite2D
 @export var ingredient = "grape"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().create_timer(5).timeout.connect(queue_free)
+	# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
