bc985caf0f7bcb0bfa056e1f6d986c816e861bf1
ryan cooper <3RCOOPER8@GMAIL.COM>
2024-02-25 16:03:47 -0500

    Final Score

--- game_manager.gd
+++ game_manager.gd
@@ -7,13 +7,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start(60)
+	timer.start(2)
 	timer.timeout.connect(_on_Timer_timeout)
 	pass # Replace with function body.
 
 
 func _on_Timer_timeout():
-	get_tree().change_scene_to_file("res://credits.tscn")
+	get_tree().change_scene_to_file("res://game_over.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timer_label.text = str(int(timer.time_left))
--- game_over.gd
+++ game_over.gd
@@ -1,5 +1,7 @@
 extends Node2D
-
+@onready var score_label = $VBoxContainer/FinalScore
+#@onready var final_score = get_tree().get_root().find_node("sample").score
+var final_score = get_node("/root/GameManager").score
 
 
 func _on_new_game_button_pressed():
@@ -10,3 +12,7 @@ func _on_new_game_button_pressed():
 
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://start_screen.tscn") # change to start screen
+
+
+func _process(delta):
+	score_label.text =  str(final_score)
