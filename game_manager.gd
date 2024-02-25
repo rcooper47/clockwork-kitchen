a097af6b82f5c25e0856b474e5d918f8893685b8
ryan cooper <3RCOOPER8@GMAIL.COM>
2024-02-25 16:28:13 -0500

    Final Score

--- game_manager.gd
+++ game_manager.gd
@@ -3,11 +3,11 @@ extends Node2D
 @onready var timer = $GameTimer
 @onready var timer_label = $TimerContainer/TimerLabel
 @onready var score_label = $ScoreContainer/ScoreLabel
-@export var score = 0
+#var score = Global.Get_Score()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start(60)
+	timer.start(10)
 	timer.timeout.connect(_on_Timer_timeout)
 	pass # Replace with function body.
 
@@ -17,5 +17,5 @@ func _on_Timer_timeout():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	timer_label.text = str(int(timer.time_left))
-	score_label.text =  str(score)
+	score_label.text =  str(Global.Get_Score())
 	#$TimerLabel.set_text(str(timer.get_time_left()))
